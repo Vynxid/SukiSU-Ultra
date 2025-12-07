c2eb9e59dd935a23412ff127eeef85c0eaafd14e
Vynxid <111081418+Vynxid@users.noreply.github.com>
2025-12-07 15:19:10 +0700

    Update selinux.h

--- kernel/selinux/selinux.h
+++ kernel/selinux/selinux.h
@@ -1,48 +1,47 @@
 #ifndef __KSU_H_SELINUX
 #define __KSU_H_SELINUX
 
-#include "linux/types.h"
-#include "linux/version.h"
-#include "linux/cred.h"
+#include <linux/types.h>
+#include <linux/version.h>
+#include <linux/cred.h>
 
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 10, 0)) ||						\
-	defined(KSU_COMPAT_HAS_SELINUX_STATE)
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 10, 0)) ||                        \
+    defined(KSU_COMPAT_HAS_SELINUX_STATE)
 #define KSU_COMPAT_USE_SELINUX_STATE
 #endif
 
 // TODO: rename to "ksu"
 #define KERNEL_SU_DOMAIN "su"
-#define KERNEL_SU_FILE "ksu_file"
+#define KERNEL_SU_FILE   "ksu_file"
 
 #define KERNEL_SU_CONTEXT "u:r:" KERNEL_SU_DOMAIN ":s0"
-#define KSU_FILE_CONTEXT "u:object_r:" KERNEL_SU_FILE ":s0"
+#define KSU_FILE_CONTEXT  "u:object_r:" KERNEL_SU_FILE ":s0"
 
-void setup_selinux(const char *);
+/* API ke bagian lain KernelSU */
 
-void setenforce(bool);
+void setup_selinux(const char *cmdline);
 
+void setenforce(bool enforcing);
 bool getenforce(void);
 
 bool is_task_ksu_domain(const struct cred *cred);
-
 bool is_ksu_domain(void);
 
 bool is_zygote(const struct cred *cred);
-
 bool is_init(const struct cred *cred);
 
 void apply_kernelsu_rules(void);
 
-u32 ksu_get_ksu_file_sid(void);
+u32  ksu_get_ksu_file_sid(void);
 
-int handle_sepolicy(unsigned long arg3, void __user *arg4);
+int  handle_sepolicy(unsigned long arg3, void __user *arg4);
 
-void setup_ksu_cred();
+void setup_ksu_cred(void);
 
 #ifdef CONFIG_KSU_SUSFS
 bool susfs_is_sid_equal(void *sec, u32 sid2);
-u32 susfs_get_sid_from_name(const char *secctx_name);
-u32 susfs_get_current_sid(void);
+u32  susfs_get_sid_from_name(const char *secctx_name);
+u32  susfs_get_current_sid(void);
 void susfs_set_zygote_sid(void);
 bool susfs_is_current_zygote_domain(void);
 void susfs_set_ksu_sid(void);
@@ -50,6 +49,6 @@ bool susfs_is_current_ksu_domain(void);
 void susfs_set_init_sid(void);
 bool susfs_is_current_init_domain(void);
 void susfs_set_priv_app_sid(void);
-#endif // #ifdef CONFIG_KSU_SUSFS
+#endif // CONFIG_KSU_SUSFS
 
-#endif
+#endif /* __KSU_H_SELINUX */
